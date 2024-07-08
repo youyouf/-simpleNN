88b865176d475d4c00e35a2b8383e0bd54dcdce9
riky_li <riky_li@foxmail.com>
2024-07-08 19:26:08 +0800

    optional

---
+++ src/optional.cpp
@@ -0,0 +1,10 @@
+#include "optional.h"
+
+namespace simpleNN{
+
+Optional::Optional()
+{
+    this->num_thread = 2;
+}
+
+}
---
+++ src/optional.h
@@ -0,0 +1,15 @@
+#ifndef _SIMPLENN_OPTIONAL_H
+#define _SIMPLENN_OPTIONAL_H
+
+namespace simpleNN{
+
+class Optional
+{
+public:
+    Optional();
+    int num_thread;
+};
+
+}
+
+#endif
